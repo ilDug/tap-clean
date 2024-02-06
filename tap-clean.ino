5232c456c453cb0249cd3aef7412418f94def724
ilDug <dognini.marco@gmail.com>
2024-02-06 22:51:03 +0100

    led contrller con i la somma degli stati

--- tap-clean.ino
+++ tap-clean.ino
@@ -17,9 +17,44 @@ const int latch = 0;  // ST_CP
 const int clock = 0;  // SH_CP
 // const int enable = 0 ; // abilita tutto il registro (quando è al GND)
 
+/** LEDS
+PROGRAMMI
+PIN     LED          P1    P2    P3    CLN     (blink del singolo led)
+---     ---          ---   ---   ---   ---
+Q0      DETERSIVO     x           x     x     (0x01)
+Q1      ANTICALCARE   x           x     x     (0x02)
+Q2      IGIENIZZANTE  x                 x     (0x04)
+Q3      AMMORBIDENTE        x           x     (0x08)
+---     ---          ---   ---   ---   ---
+HEX                  0x07  0x08  0x03 0x0F
+------------------------------------------
+*/
+uint8_t P1 = 0x07; 
+uint8_t P2 = 0x08;
+uint8_t P3 = 0x03;
+uint8_t CLN = 0x0f;
+uint8_t OFF = 0x00;
+uint8_t BLK0 = 0x01;
+uint8_t BLK1 = 0x02;
+uint8_t BLK2 = 0x04;
+uint8_t BLK3 = 0x08;
 
-// dichiaraazione FUNZIONI
-void ledCtrl(byte value);
+/**
+LIVELLI
+PIN     LED          SM    MD    LG    
+---     ---          ---   ---   ---   
+Q4      PWR           x     x     x
+Q5      LVL1          x
+Q6      LVL2          x     x
+Q7      LVL3          x     x     x
+---     ---          ---   ---   ---   
+HEX                  0x0F  0x0D  0x09     
+------------------------------------------
+*/
+uint8_t  SM = 0x0f;
+uint8_t  MD = 0x0d;
+uint8_t  LG = 0x09;
+
 
 String version = "[v0.0.1]";
 void setup() {
@@ -31,50 +66,19 @@ void setup() {
   pinMode(latch, OUTPUT);
   pinMode(data, OUTPUT);
 
-  //
+  // inizializza i led
+  ledCtrl(P1, MD);
+
+
 }
 
 
 void loop() {
   delay(50);
-  ledCtrl(0x08);
+  ledCtrl(P1, MD);
 }
 
-/** LEDS
-GRUPPO I
-PIN     LED          P1    P2    P3    CLN     (blink del singolo led)
----     ---          ---   ---   ---   ---
-Q0      DETERSIVO     x           x     x     (0x01)
-Q1      ANTICALCARE   x           x     x     (0x02)
-Q2      IGIENIZZANTE  x                 x     (0x04)
-Q3      AMMORBIDENTE        x           x     (0x08)
----     ---          ---   ---   ---   ---
-HEX                  0x07  0x08  0x03 0x0F
-------------------------------------------
-*/
-enum Programs {
-  P1 = 0x07,
-  P2 = 0x08,
-  P3 = 0x03,
-  CLN = 0x0f
-};
-/**
-GRUPPO II
-PIN     LED          SM    MD    LG    
----     ---          ---   ---   ---   
-Q4      PWR           x     x     x
-Q5      LVL1          x
-Q6      LVL2          x     x
-Q7      LVL3          x     x     x
----     ---          ---   ---   ---   
-HEX                  0x0F  0x0D  0x09     
-------------------------------------------
-*/
-enum Levels {
-  SM = 0x0f,
-  MD = 0x0d,
-  LG = 0x09,
-};
+
 
 /**
 Per sommare il risultato devo fare 
@@ -83,17 +87,13 @@ Per sommare il risultato devo fare
 uint8_t x = 0x0f << 4;
 uint8_t y = x + 0x07;
 y => 1111 0111
-
-oppure 
-Levels lvl = LOW;
-uint8_t x = lvl << 4;
-Programs prg = P2;
-uint8_t y = x + prg;
-y => 1111 0111
 */
 
-void ledCtrl(byte value) {
-  // byte value = 0x01;  // 0000 0001
+
+// scrive il valore nello SHIFT REGISTER per accendere i led
+uint8_t ledCtrl(uint8_t program, uint8_t level) {
+  uint8_t x = level << 4;
+  uint8_t value = x + program;
   digitalWrite(latch, LOW);                // attiva la scrittura dei dati (quando è LOW)
   shiftOut(data, clock, MSBFIRST, value);  // scrive i dati  (MSBFIRST è l'ordine dei dati dalla cifra che pesa di più)
   digitalWrite(latch, HIGH);               // disabilita la scrittura dei dati (quando è HIGH)
