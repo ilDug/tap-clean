d6b3685be5d5371a3d114eb81aa2fafe8c810a7e
ilDug <dognini.marco@gmail.com>
2024-02-14 11:48:36 +0100

    aggiutno la misura della distanza ultrasonica

--- tap-door/tap-door.ino
+++ tap-door/tap-door.ino
@@ -11,15 +11,16 @@ const int MOTOR_PIN_2 = 0;
 const int MOTOR_PIN_3 = 0;
 const int MOTOR_PIN_4 = 0;
 
-const int TRIGGER_PIN = 9;
-const int ECO_PIN = 10;
+const int TRIGGER_PIN = 0;
+const int ECO_PIN = 0;
 
 const int STP = 200;  // step per revolution del motore stepper.
 const int DOOR_LIM_SWITCH = 0;
 const int TOGGLE_BTN_PIN = 0;
 
 const int WAIT = 1000 * 1;
-const int LIMIT = 0;
+const float LIMIT = 10;  // cm
+
 
 Stepper motor(STP, MOTOR_PIN_1, MOTOR_PIN_2, MOTOR_PIN_3, MOTOR_PIN_4);
 Door door(&motor);
@@ -33,6 +34,8 @@ void setup() {
 
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECO_PIN, INPUT);
+  digitalWrite(TRIGGER_PIN, LOW);
+
   pinMode(DOOR_LIM_SWITCH, INPUT_PULLUP);
   pinMode(TOGGLE_BTN_PIN, INPUT_PULLUP);
 
@@ -42,12 +45,7 @@ void setup() {
 
 void loop() {
 
-  // misurare la distanza;
-  // se la distanza è minore di una costante,
-  // azionare il toggle
-  // restare in attesa per il tempo che uno toglie la mano
-
-  while (isClose()) {  // sta fermo fino a quando non tolgo la mano
+  while (distance() < LIMIT) {  // sta fermo fino a quando non tolgo la mano
     FIRED = true;
     delay(10);
   }
@@ -63,11 +61,17 @@ void loop() {
 }
 
 
-bool isClose(void) {
-  // misura
-  // se è sotto il limite restitusce true altrimenti false
+// misura la distanza in cm
+float distance() {
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  unsigned long time = pulseIn(ECO_PIN, HIGH);
+  float length = 0.03431 * time / 2;  // 0.03431 = MACH in cm/microsec [ divido per due per chè il percorso dell'onda è andata + ritorno]
 }
 
+
+// apri-chiudi
 void toggle(void) {
   door.toggle();
 }
